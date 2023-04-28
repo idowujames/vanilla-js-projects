35ea5e906724124ef72a999ccdfbe1f204a34c3c
James <jamesid2915@gmail.com>
2023-04-28 02:28:38 +0100

    Fixed api call errors to project 1 - weather app

--- Project1 - Weather App/index.js
+++ Project1 - Weather App/index.js
@@ -1,4 +1,4 @@
-const apikey = "86d833284ccb320afdad2da3c5f55dc9";
+const apikey = "API_KEY"; // Openweathermap API KEY
 
 const weatherDataEl = document.getElementById("weather-data");
 
@@ -16,9 +16,10 @@ formEl.addEventListener("submit", (event)=>{
 
 async function getWeatherData(cityValue){
     try {
-        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${apikey}$units=metric`)
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${apikey}&units=metric`)
 
         if (response.ok) {
+            console.log("connection made")
             const data = await response.json();
           
             const temperature = Math.round(data.main.temp);
@@ -30,7 +31,7 @@ async function getWeatherData(cityValue){
               `Wind Speed: ${data.wind.speed} m/s`,
             ];
           
-            weatherDataEl.querySelector(".icon").innerHTML = `<img src="http://openweathermap.org/img/wn/${icon}.png" alt="Weather Icon">`;
+            weatherDataEl.querySelector(".icon").innerHTML = `<img src="https://openweathermap.org/img/wn/${icon}.png" alt="Weather Icon">`;
             weatherDataEl.querySelector(".temperature").textContent = `${temperature}°C`;
             weatherDataEl.querySelector(".description").textContent = description;
             weatherDataEl.querySelector(".details").innerHTML = details.map((detail) => `<div>${detail}</div>`).join("");
@@ -42,7 +43,7 @@ async function getWeatherData(cityValue){
     } catch (error) {
         weatherDataEl.querySelector(".icon").innerHTML = "";
   
-        weatherDataEl.querySelector(".temprature").textContent = "";
+        weatherDataEl.querySelector(".temperature").textContent = "";
         weatherDataEl.querySelector(".description").textContent ="An Error Happened please try again later";
 
         weatherDataEl.querySelector(".details").innerHTML = "";
@@ -51,41 +52,3 @@ async function getWeatherData(cityValue){
 
 
 
-// async function getWeatherData(cityValue){
-//     try {
-//         const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${apikey}$units=metric`)
-
-//         if(!response.ok){
-//             throw new Error("Networ response was not ok")
-//         }
-
-//         const data = await response.json();
-
-//         const temperature = Math.round(data.main.temp);
-
-//         const description = data.weather[0].description;
-
-//         const icon = data.weather[0].icon;
-
-//         const details = [
-//             `Feels like: ${Math.round(data.main.feels_like)}°C`,
-//             `Humidity: ${data.main.humidiy}%`,
-//             `Wind Speed: ${data.wind.speed} m/s`,
-//         ];
-
-//         weatherDataEl.querySelector(".icon").innerHTML = `<img src="http://openweathermap.org/img/wn/${icon}.png" alt="Weather Icon">`;
-  
-//         weatherDataEl.querySelector(".temprature").textContent =`${temperature}°C`;
-//         weatherDataEl.querySelector(".description").textContent =`${description}°C`;
-
-//         weatherDataEl.querySelector(".details").innerHTML = details.map((details)=>`<div>${details}</div>`).join("");
-  
-//     } catch (error) {
-//         weatherDataEl.querySelector(".icon").innerHTML = "";
-  
-//         weatherDataEl.querySelector(".temprature").textContent = "";
-//         weatherDataEl.querySelector(".description").textContent ="An Error Happened please try again later";
-
-//         weatherDataEl.querySelector(".details").innerHTML = "";
-//     }
-// }
